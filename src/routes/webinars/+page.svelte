389b0ac45ce8ccb1066171c95e003386ba419184
zoepje <zoe.penner@hva.nl>
2024-10-14 15:23:28 +0200

    overzetten repo deel 8

---
+++ src/routes/webinars/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+  import {Search, Filter, WebinarOverview} from "$lib/index.js";
+  export let data
+</script>
+
+<main>
+  <h1>Webinars</h1>
+
+  <Search />
+
+  <Filter />
+
+  <ul>
+    {#each data.webinars as webinar}
+    <li>
+      <WebinarOverview {...webinar}/>
+    </li>
+    {/each}
+  </ul>
+</main>
+
+<style>
+  main {
+    width: 100vw;
+  }
+
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 2em;
+    margin-left: auto;
+  }
+</style>
