ce61d009bed60011bdbc74331d8bee9f2f210a10
zoepje <zoe.penner@hva.nl>
2024-11-12 18:29:28 +0100

    start contouring detail page https://github.com/fdnd-agency/avl/issues/38

---
+++ src/routes/contourings/[slug]/+page.server.js
@@ -0,0 +1,13 @@
+import fetchJson from "$lib/fetch-json"
+
+const baseURL = 'https://fdnd-agency.directus.app/items/'
+
+export async function load({params}) {
+  const url = `${baseURL}/avl_contourings?fields=*.*.*&filter[slug][_eq]=${params.slug}`
+
+  const contouring = await fetchJson(url)
+  
+  return {
+    contouring: contouring.data[0]
+  }
+}
---
+++ src/routes/contourings/[slug]/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let data; 
+</script>
+
+
+<h1>{data.contouring.title}</h1>
+
+<img src="https://fdnd-agency.directus.app/assets/{data.contouring.image_scan.id}" alt="CT-Scans" width="384" height="384" />
+
+<style>
+  img {
+    width: 80vw;
+  }
+</style>
