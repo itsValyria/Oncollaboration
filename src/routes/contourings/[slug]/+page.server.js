06466d1db59c3d304041307a8af0f544f8ae3c31
zoepje <zoe.penner@hva.nl>
2024-11-15 13:26:22 +0100

    url fetch corectie

--- src/routes/contourings/[slug]/+page.server.js
+++ src/routes/contourings/[slug]/+page.server.js
@@ -3,7 +3,7 @@ import fetchJson from "$lib/fetch-json"
 const baseURL = 'https://fdnd-agency.directus.app/items/'
 
 export async function load({params}) {
-  const url = `${baseURL}/avl_contourings?fields=*.*.*&filter[slug][_eq]=${params.slug}`
+  const url = `${baseURL}avl_contourings?fields=*.*.*&filter[slug][_eq]=${params.slug}`
 
   const contouring = await fetchJson(url)
   
