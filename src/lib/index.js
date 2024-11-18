a04df0c909bcb14f46f3b2067590d330d592f472
Zoë <144004461+zoepje@users.noreply.github.com>
2024-11-18 23:24:05 +0100

    Fix Vercel deployment

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,7 @@
-import ContouringOverview from "./blocks/contouring-overview.svelte";
-import Search from "./blocks/search.svelte";
-import Filter from "./blocks/filter.svelte";
-import WebinarOverview from "./blocks/webinar-overview.svelte";
-import Navigation from "./constructions/navigation.svelte";
+import ContouringOverview from "./Blocks/contouring-overview.svelte";
+import Search from "./Blocks/search.svelte";
+import Filter from "./Blocks/filter.svelte";
+import WebinarOverview from "./Blocks/webinar-overview.svelte";
+import Navigation from "./Constructions/navigation.svelte";
 
 export {ContouringOverview, Search, Filter, WebinarOverview, Navigation}
