5efe6883e1bdfd2a7aec6c81c80758f186c0fc46
Valyria <76444716+itsValyria@users.noreply.github.com>
2024-11-18 14:49:29 +0100

    put it in a component wiehoe

---
+++ src/lib/search-results.svelte
@@ -0,0 +1,62 @@
+<script>
+  export let data;
+  import { ContouringOverview, WebinarOverview } from "$lib/index.js";
+</script>
+
+<main>
+  {#if data.webinars.length > 0}
+    <h2>Matching Webinars</h2>
+    <div class="carrousel">
+      {#each data.webinars as webinar}
+        <div>
+          <WebinarOverview {...webinar} />
+        </div>
+      {/each}
+    </div>
+  {/if}
+
+  {#if data.contourings.length > 0}
+    <h2>Matching Contourings</h2>
+    <div class="carrousel">
+      {#each data.contourings as contouring}
+        <div>
+          <ContouringOverview {...contouring} />
+        </div>
+      {/each}
+    </div>
+  {/if}
+
+  {#if data.webinars.length === 0 && data.contourings.length === 0}
+    <p>No results found.</p>
+  {/if}
+</main>
+
+<style>
+  main {
+    margin-top: 2rem;
+  }
+
+  .carrousel {
+    display: flex;
+    overflow-x: auto;
+    scroll-snap-type: x mandatory;
+    scroll-padding-left: 0px;
+    -webkit-overflow-scrolling: touch;
+    scroll-behavior: smooth;
+    margin: 2em auto;
+    gap: 10px;
+  }
+
+  .carrousel > div {
+    flex: 0 0 auto;
+    scroll-snap-align: start;
+  }
+
+  @media screen and (min-width: 1599px) {
+    .carrousel {
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      gap: 2rem;
+    }
+  }
+</style>
--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,38 +1,14 @@
 <script>
   export let data;
   import { ContouringOverview, Search, WebinarOverview } from "$lib/index.js";
+  import SearchResults from "../../lib/search-results.svelte";
 </script>
 
 <main>
   <h1>Search Results</h1>
   <Search></Search>
   <p>Results for: <strong>"{data.query}"</strong></p>
-  
-  {#if data.webinars.length > 0}
-    <h2>Matching Webinars</h2>
-    <div class="carrousel">
-      {#each data.webinars as webinar}
-        <div>
-          <WebinarOverview {...webinar} />
-        </div>
-      {/each}
-    </div>
-  {/if}
-
-  {#if data.contourings.length > 0}
-    <h2>Matching Contourings</h2>
-    <div class="carrousel">
-      {#each data.contourings as contouring}
-        <div>
-          <ContouringOverview {...contouring} />
-        </div>
-      {/each}
-    </div>
-  {/if}
-
-  {#if data.webinars.length === 0 && data.contourings.length === 0}
-    <p>No results found.</p>
-  {/if}
+  <SearchResults {data} />
 </main>
 
 <style>
@@ -41,31 +17,9 @@
     margin-top: 2rem;
   }
 
-  .carrousel {
-    display: flex;
-    overflow-x: auto;
-    scroll-snap-type: x mandatory;
-    scroll-padding-left: 0px;
-    -webkit-overflow-scrolling: touch;
-    scroll-behavior: smooth;
-    margin: 2em auto;
-    gap: 10px;
-  }
-
-  .carrousel > div {
-    flex: 0 0 auto;
-    scroll-snap-align: start;
-  }
-
   @media screen and (min-width: 1599px) {
     main {
       margin: 4rem;
-    }
-
-    .carrousel {
-      display: grid;
-      grid-template-columns: repeat(4, 1fr);
-      gap: 2rem;
     }
   }
 </style>
