0dabec27a9efd8b565ba9a6c3b610e8a282cc867
zoepje <zoe.penner@hva.nl>
2024-11-18 16:10:22 +0100

    QandA verplaats

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,8 @@
-import ContouringOverview from "./blocks/contouring-overview.svelte";
-import Search from "./blocks/search.svelte";
-import Filter from "./blocks/filter.svelte";
-import WebinarOverview from "./blocks/webinar-overview.svelte";
-import Navigation from "./constructions/navigation.svelte";
+import ContouringOverview from "./Blocks/contouring-overview.svelte";
+import Search from "./Blocks/search.svelte";
+import Filter from "./Blocks/filter.svelte";
+import WebinarOverview from "./Blocks/webinar-overview.svelte";
+import Navigation from "./Constructions/navigation.svelte";
+import QandA from "./Assemblages/q-a.svelte";
 
 export {ContouringOverview, Search, Filter, WebinarOverview, Navigation, QandA}
