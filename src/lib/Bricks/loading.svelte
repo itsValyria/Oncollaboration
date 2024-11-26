33f661a21945dd19661470de38ac9a69e828da31
zoepje <zoe.penner@hva.nl>
2024-11-26 15:34:24 +0100

    PE voor de loader

--- src/lib/Bricks/loading.svelte
+++ src/lib/Bricks/loading.svelte
@@ -1,6 +1,12 @@
-<div class="loader"></div>
+<div class="loader">
+  <p>loading</p>
+</div>
 
 <style>
+  p {
+    display: none;
+  }
+
   .loader {
     margin: auto;
     width: 15px;
@@ -8,6 +14,7 @@
     border-radius: 50%;
     animation: l5 1s infinite linear alternate;
   }
+  
   @keyframes l5 {
     0%  {box-shadow: 20px 0 #fff, -20px 0 #fff2; background: #fff }
     33% {box-shadow: 20px 0 #fff, -20px 0 #fff2; background: #fff2}
