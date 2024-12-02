62302551ea304f931d58c2d6bed58be25d128ade
Ryank2004 <ryankorver04@gmail.com>
2024-12-02 12:09:00 +0100

    Aria-labels buttons

--- src/lib/Blocks/filter.svelte
+++ src/lib/Blocks/filter.svelte
@@ -5,14 +5,14 @@
 
 <form method="get" action="/filter" id="filter" name="filter">
   <button type="button" on:click={() => goto('/webinars')} class:selected={!activeCategory || activeCategory === 'all'}>All</button>
-  <button type="submit" name="category" value="breast" class:selected={activeCategory === 'breast'}>Breast</button>
-  <button type="submit" name="category" value="gastrointestinal" class:selected={activeCategory === 'gastrointestinal'}>Gastrointestinal</button>
-  <button type="submit" name="category" value="gynaecology" class:selected={activeCategory === 'gynaecology'}>Gynaecology</button>
-  <button type="submit" name="category" value="head&neck" class:selected={activeCategory === 'head&neck'}>Head & neck</button>
-  <button type="submit" name="category" value="lung" class:selected={activeCategory === 'lung'}>Lung</button>
-  <button type="submit" name="category" value="neuro-oncology" class:selected={activeCategory === 'neuro-oncology'}>Neuro-oncology</button>
-  <button type="submit" name="category" value="prostate" class:selected={activeCategory === 'prostate'}>Prostate</button>
-  <button type="submit" name="category" value="urology" class:selected={activeCategory === 'urogoly'}>Urology</button>
+  <button type="submit" name="category" value="breast" aria-label="Filter on breast category" class:selected={activeCategory === 'breast'}>Breast</button>
+  <button type="submit" name="category" value="gastrointestinal" aria-label="Filter on gastrointestinal category" class:selected={activeCategory === 'gastrointestinal'}>Gastrointestinal</button>
+  <button type="submit" name="category" value="gynaecology" aria-label="Filter on gynaecology category" class:selected={activeCategory === 'gynaecology'}>Gynaecology</button>
+  <button type="submit" name="category" value="head&neck" aria-label="Filter on head and neck category" class:selected={activeCategory === 'head&neck'}>Head & neck</button>
+  <button type="submit" name="category" value="lung" aria-label="Filter on lung category" class:selected={activeCategory === 'lung'}>Lung</button>
+  <button type="submit" name="category" value="neuro-oncology" aria-label="Filter on neuro oncology category" class:selected={activeCategory === 'neuro-oncology'}>Neuro-oncology</button>
+  <button type="submit" name="category" value="prostate" aria-label="Filter on prostate category" class:selected={activeCategory === 'prostate'}>Prostate</button>
+  <button type="submit" name="category" value="urology" aria-label="Filter on urology category" class:selected={activeCategory === 'urogoly'}>Urology</button>
 </form>
 
 <!-- 
