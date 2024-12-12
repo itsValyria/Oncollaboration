ad5e3bdf0b3694230c2c1a16d048e4ad53751cfa
zoepje <zoe.penner@hva.nl>
2024-12-12 11:52:29 +0100

    put fetch-replies in lib
    you reuse it

--- src/routes/contourings/[slug]/+page.server.js
+++ src/routes/contourings/[slug]/+page.server.js
@@ -1,21 +1,7 @@
 import fetchJson from "$lib/fetch-json"
+import fetchReplies from "$lib/fetch-replies"
 
 const baseURL = 'https://fdnd-agency.directus.app/items/'
-
-async function fetchReplies(commentIds) {
-  if (!commentIds || commentIds.length === 0) return [];
-
-  // Fetch reply data for the given IDs
-  const repliesURL = `${baseURL}avl_comments?fields=time_posted,content,parent_id,likes,id,user_id.profile_picture.id,user_id.profile_picture.title,user_id.fullname,replies&filter[id][_in]=${commentIds.join(',')}`;
-  const replies = await fetchJson(repliesURL);
-  
-  // Recursively fetch replies for each comment
-  for (const reply of replies.data) {
-    reply.replies = await fetchReplies(reply.replies); // Recursive fetching
-  }
-
-  return replies.data;
-}
 
 export async function load({params}) {
   const contouringURL = `${baseURL}avl_contourings?fields=image_scan,title,id,user_id.profile_picture.id,user_id.profile_picture.title,user_id.fullname,user_id.entitle,used_literature.directus_files_id,categories.avl_categories_id.name&filter[slug][_eq]=${params.slug}`
