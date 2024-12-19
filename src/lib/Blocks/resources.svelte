23d90fe832e65c873b773ec9b34692ce8715a238
zoepje <zoe.penner@hva.nl>
2024-12-19 19:57:42 +0100

    aria-label download link

--- src/lib/Blocks/resources.svelte
+++ src/lib/Blocks/resources.svelte
@@ -40,7 +40,7 @@
           {#if resource.directus_files_id.type !== "application/zip"}
             <PreviewDoc id = {resource.directus_files_id.id}/>
           {/if}
-          <a href="https://fdnd-agency.directus.app/assets/{resource.directus_files_id.id}?download" download="test" target="_blank">
+          <a href="https://fdnd-agency.directus.app/assets/{resource.directus_files_id.id}?download" download="test" target="_blank" aria-label="download {resource.directus_files_id.title}">
             <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
               <path d="M7.67213 10.2297L4.47541 7.03303L5.37049 6.10598L7.03278 7.76827V2.55762H8.31147V7.76827L9.97377 6.10598L10.8688 7.03303L7.67213 10.2297ZM3.83606 12.7871C3.48442 12.7871 3.1834 12.6619 2.93299 12.4115C2.68258 12.1611 2.55737 11.8601 2.55737 11.5084V9.5904H3.83606V11.5084H11.5082V9.5904H12.7869V11.5084C12.7869 11.8601 12.6617 12.1611 12.4113 12.4115C12.1609 12.6619 11.8598 12.7871 11.5082 12.7871H3.83606Z"/>
             </svg> 
