96c9ccf847902a5d93eb8b08badb4a33c70e02a6
zoepje <zoe.penner@hva.nl>
2025-01-14 15:28:07 +0100

    white space
    #93

--- src/lib/Bricks/loader-spin.svelte
+++ src/lib/Bricks/loader-spin.svelte
@@ -1,7 +1,5 @@
 <div class="load">
-  <p>
-    loading
-  </p>
+  <p>Loading</p>
 </div>
 
 <style>
@@ -16,13 +14,12 @@
     aspect-ratio: 1;
     border-radius: 50%;
     background: var(--primary-color);
-    --_m: 
-      conic-gradient(#0000 10%,#000),
+    --_m: conic-gradient(#0000 10%,#000),
       linear-gradient(#000 0 0) content-box;
     -webkit-mask: var(--_m);
-            mask: var(--_m);
+    mask: var(--_m);
     -webkit-mask-composite: source-out;
-            mask-composite: subtract;
+    mask-composite: subtract;
     animation: l3 1s infinite linear;
   }
 
